2e28e51f08c2fba868d00a6a41283c18e30b7d18
a-bills <121698448+a-bills@users.noreply.github.com>
2023-08-02 13:56:20 -0400

    Organize boxes into grid layout

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
-const container = document.querySelector('#gridContainer');
-for (let i = 0; i < 100; i++) {
-    const box = document.createElement('div');
+const container = document.querySelector('#grid-container');
+const gridSize = 16;
+const box = document.createElement('div');
+for (let i = 0; i < (gridSize * gridSize); i++) {
     box.classList.add('box');
+    box.style.flexBasis = `${800 / gridSize}px`;
     container.appendChild(box);
 }
