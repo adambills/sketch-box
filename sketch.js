57479edff5773c06d10d28d60e4bb302257742bb
a-bills <121698448+a-bills@users.noreply.github.com>
2023-08-02 16:11:31 -0400

    Add option for user to change grid size

--- sketch.js
+++ sketch.js
@@ -1,20 +1,41 @@
 const container = document.querySelector('#grid-container');
-let gridSize = 16;
+const gridButton = document.querySelector('#grid-size');
 
-for (let i = 0; i < (gridSize * gridSize); i++) {
-    const box = document.createElement('div');
-    box.classList.add('box');
-    box.style.flexBasis = `${800 / gridSize}px`;
-    container.appendChild(box);
-}
+gridButton.addEventListener('click', () => updateGridSize());
 
-const boxes = document.querySelectorAll('.box');
-
-for (const box of boxes) {
-    box.addEventListener('mouseover', 
-        (event) => {updateBoxColor(event)}
+function updateGridSize() {
+    let gridSize = parseInt(
+        prompt('Enter desired grid size. Must be a number > 0 & <= 100: ', '16')
     );
+    if (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+        alert('Error! Please enter an appropriate value.');
+        updateGridSize();
+    }
+    if (!(isNaN(gridSize) || gridSize < 1 || gridSize > 100)) {
+        generateGrid(gridSize);
+    }
 }
+
+function generateGrid(gridSize) {
+    container.replaceChildren();
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.style.flexBasis = `${800 / gridSize}px`;
+        container.appendChild(box);
+    }
+    startDrawing();
+}
+
+function startDrawing() {
+    const boxes = document.querySelectorAll('.box');
+    for (const box of boxes) {
+        box.addEventListener('mouseover',
+            (event) => { updateBoxColor(event) }
+        );
+    }
+}
+
 
 function updateBoxColor(event) {
     if (event.buttons === 1) {
@@ -24,3 +45,4 @@ function updateBoxColor(event) {
     }
 }
 
+generateGrid(16);
