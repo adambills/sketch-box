ff8f2eb55b85acc0eb4ac33118314136f679d05f
a-bills <121698448+a-bills@users.noreply.github.com>
2023-08-02 14:56:03 -0400

    Change color of boxes on mouseover and mousedown

--- sketch.js
+++ sketch.js
@@ -1,8 +1,26 @@
 const container = document.querySelector('#grid-container');
-const gridSize = 16;
-const box = document.createElement('div');
+let gridSize = 16;
+
 for (let i = 0; i < (gridSize * gridSize); i++) {
+    const box = document.createElement('div');
     box.classList.add('box');
     box.style.flexBasis = `${800 / gridSize}px`;
     container.appendChild(box);
 }
+
+const boxes = document.querySelectorAll('.box');
+
+for (const box of boxes) {
+    box.addEventListener('mouseover', 
+        (event) => {updateBoxColor(event)}
+    );
+}
+
+function updateBoxColor(event) {
+    if (event.buttons === 1) {
+        let box = event.target;
+        box.style.backgroundColor = 'black';
+        console.log(event);
+    }
+}
+
